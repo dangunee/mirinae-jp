831339f54bd2bb92a7aad930cc95482ecf0fcaff
dangunee <dangunee@gmail.com>
2026-03-15 00:22:10 +0900

    favicon 추가, group/special 등 페이지 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ const MIRINAE_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://mirinae-jp
 export const metadata = {
   title: "ミリネ韓国語教室",
   description: "ミリネ韓国語教室 | 東京で韓国語教室をお探しならミリネ。",
+  icons: { icon: "/favicon.png" },
   openGraph: {
     title: "ミリネ韓国語教室 | 東京で韓国語教室をお探しならミリネ",
     description: "「話せる」を、ここで実現する。経験豊富な講師陣と、個人に合わせた多彩な講座であなたの韓国語を、確実に伸ばします。",
